53af73374f18973b56a6e9c3ac82bca393a07bc5
Unknown <nicoo.oliver@gmail.com>
2018-09-18 21:34:46 -0500

    Début projet - Menu

---
+++ src/main/java/BattleShipMain.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BattleShipMain {
+	public static void main(String[] args) {
+		String reponse;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.printf("Début du jeu : Jouer contre un ordinateur ? (o/n) : %nQuitter (q)%n ");
+			reponse = sc.nextLine();
+		} while (!reponse.toString().equals("o") && !reponse.toString().equals("O")
+				&& !reponse.toString().equals("n") && !reponse.toString().equals("N")
+				&& !reponse.toString().equals("q") && !reponse.toString().equals("Q"));
+		if(reponse.toString().equals("q") || reponse.toString().equals("Q")) {
+			System.out.println("Au revoir !");
+			return;
+		}
+		if(reponse.toString().equals("o") || reponse.toString().equals("O")) {
+			//Start game
+		}
+		else {
+			//Start game contre joueur
+		}
+	}
+}
